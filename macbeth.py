978bac4fdc67f24d704e63b826b9ffaf27e6574a
varellalexandre <varellalexandre@gmail.com>
2020-12-05 22:24:14 -0300

    atualizando restrições programa 1

--- macbeth.py
+++ macbeth.py
@@ -2,7 +2,7 @@ from macbeth_interfaces import *
 
 
 class Program_MC1_Vars:
-    def __init__(self,solver:pywraplp.Solver):
+    def __init__(self,solver:pywraplp.Solver,set:list):
         self.s0 = solver.NumVar(0,0,'s0')
         self.s1 = solver.NumVar(1,1,'s1')
         for category in range(2,6):
@@ -18,6 +18,17 @@ class Program_MC1_Vars:
             )
         self.theta = solver.NumVar(0,solver.infinity(),'theta')
         self.Cmin = solver.NumVar(0,solver.infinity(),'Cmin')
+        for element in set:
+            var = solver.NumVar(
+                -solver.infinity(),
+                solver.infinity(),
+                element
+            )
+            setattr(
+                self,
+                element,
+                var
+            )
 
 class Program_MC1_Constraints:
     def __init__(
@@ -27,6 +38,30 @@ class Program_MC1_Constraints:
         set:pd.DataFrame
     ):
         ordered_prefs = [row[0] for idx,row in set.iterrows()]
+        for category in range(2,6):
+            var_k = getattr(
+                vars,
+                's{category}'.format(category=category)
+            )
+            var_k_1 = getattr(
+                vars,
+                's{category}'.format(category=category-1)
+            )
+            constr = solver.Constraint(
+                1,
+                solver.infinity()
+            )
+            constr.SetCoefficient(var_k,1)
+            constr.SetCoefficient(var_k_1,-1)
+            setattr(
+                self,
+                'c2_s{category}_s{category2}'.format(
+                    category=category,
+                    category2=category-1
+                ),
+                constr
+            )
+
         for x,name_x in enumerate(ordered_prefs):
             next = x+1
             for y,name_y in enumerate(ordered_prefs[next:]):
@@ -46,12 +81,14 @@ class Program_MC1:
 
 
     def schematize(self):
-        vars = self.vars_object(self.solver)
+        ordered_prefs = [row[0] for idx,row in self.set.iterrows()]
+        vars = self.vars_object(self.solver,ordered_prefs)
         constraints = self.constraints_object(
             self.solver,
             vars,
             self.set,
         )
+        print(len(self.solver.constraints()))
 
 
 class Macbeth:
